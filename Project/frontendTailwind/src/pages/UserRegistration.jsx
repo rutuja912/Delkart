1ce700b0f10b4f639beebc3e0e2bda3160fb8ef5
rutuja912 <s.rutuja912@gmail.com>
2025-06-22 00:07:21 +0530

    claude changes

--- Project/frontendTailwind/src/pages/UserRegistration.jsx
+++ Project/frontendTailwind/src/pages/UserRegistration.jsx
@@ -30,7 +30,7 @@ function UserRegistration() {
                 data-name="Layer 1"
                 xmlns="http://www.w3.org/2000/svg"
                 width="100%"
-                height="auto"
+                height="100%"
                 viewBox="0 0 744.84799 747.07702"
               >
                 <path
